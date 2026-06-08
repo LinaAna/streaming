4b5378c00754672ea9ebda823ce22b04445f7d03
LinaAna <linacarolinadesouza1107@gmail.com>
2026-06-08 11:22:30 -0300

    novo projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './shared/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "navbar", component: Navbar},
+    {path: '',redirectTo: "navbar", pathMatch:'full'}
+];
